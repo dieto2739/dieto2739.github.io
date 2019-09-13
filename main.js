49c26de09ba5526e3ff184eb8ec041c2da1de63b
dieto2739 <55242891+dieto2739@users.noreply.github.com>
2019-09-13 10:49:18 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 $('.carousel').carousel()
+
+$(document).scroll(function() {
+
+    var scrollTop = $(window).scrollTop();
+            console.log(scrollTop);
+
+          if (scrollTop >= 2000 ) {
+              $('#menu').addClass("transparent");
+          }
+          else{
+              $('#menu').removeClass("transparent");
+          }
+  });
